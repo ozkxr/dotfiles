88fbec6e790169dc1ad81ec1eba5e0dcde4d1301
Oscar Menéndez <omenendez7@gmail.com>
2019-07-11 22:28:18 -0600

    Add fish config files

---
+++ fish/config.fish
@@ -0,0 +1,16 @@
+nvm use 10.15
+
+set -gx PATH /usr/local/go/bin $PATH
+
+function fish_prompt
+    set_color brcyan 
+    set_color -r
+    echo -n -s "$USER" @ (prompt_hostname)
+    set_color normal
+    echo -n -s ' '
+    set_color brcyan
+    echo -n -s "$__fish_prompt_cwd" (prompt_pwd) (__fish_vcs_prompt) "$__fish_prompt_normal" '> '
+    set_color normal
+end
+
+set fish_greeting
