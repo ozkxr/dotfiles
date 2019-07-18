da39e51de4ae7a55e1a2a3afcd79e860aac9d173
Oscar Menéndez <omenendez7@gmail.com>
2019-07-17 22:21:31 -0600

    Update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -7,10 +7,23 @@ function fish_prompt
     set_color -r
 #   echo -n -s "$USER" @ (prompt_hostname)
     set_color normal
-    echo -n -s ' '
+#    echo -n -s ' '
     set_color brcyan
     echo -n -s "$__fish_prompt_cwd" (prompt_pwd) (__fish_vcs_prompt) "$__fish_prompt_normal" '> '
     set_color normal
 end
 
 set fish_greeting
+
+function clear
+    printf "\ec"
+end
+
+if test -d ~/.cargo/bin
+    set -gx PATH $HOME/.cargo/bin $PATH
+end
+
+if status is-interactive
+and not set -q TMUX
+    exec tmux
+end
