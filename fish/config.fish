75c7ad50447c8e557f30dde244833b304313d1d6
Oscar Menéndez <omenendez7@gmail.com>
2019-07-19 17:52:41 -0600

    Clean and resort fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,29 +1,31 @@
-nvm use 10.15
+# Disable initial shell greeting
+set fish_greeting
 
-set -gx PATH /usr/local/go/bin $PATH
+# Start tmux, if not already started
+# if status is-interactive
+# and not set -q TMUX
+#     exec tmux
+# end
 
+# Use a simpler prompt
 function fish_prompt
-    set_color brcyan 
-    set_color -r
-#   echo -n -s "$USER" @ (prompt_hostname)
-    set_color normal
-#    echo -n -s ' '
     set_color brcyan
     echo -n -s "$__fish_prompt_cwd" (prompt_pwd) (__fish_vcs_prompt) "$__fish_prompt_normal" '> '
     set_color normal
 end
 
-set fish_greeting
+# Select nvm version
+nvm use 10.15
 
-function clear
-    printf "\ec"
+# Add golang and rust to path
+if test -d /usr/local/go/bin
+   set -gx PATH /usr/local/go/bin $PATH
 end
 
 if test -d ~/.cargo/bin
     set -gx PATH $HOME/.cargo/bin $PATH
 end
 
-if status is-interactive
-and not set -q TMUX
-    exec tmux
-end
+# Aliases 
+alias clear="printf "\ec""
+alias vim="nvim"
